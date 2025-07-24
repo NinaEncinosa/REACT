f5ab6e9d4cfedb405ad8613de0ed9d0959be6f8a
nencinosa <ninaencinosacastro@gmail.com>
2025-07-24 13:29:55 -0600

    1.8 Create new component Statistics

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const StatisticLine = ({ text, value }) => (
   <p>
     {text}
@@ -7,15 +9,26 @@ const StatisticLine = ({ text, value }) => (
   </p>
 );
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total ? (good - bad) / total : 0;
+  const positive = total ? (good * 100) / total : 0;
+  return (
+    <>
+      <StatisticLine text={"good "} value={good}></StatisticLine>
+      <StatisticLine text={"neutral "} value={neutral}></StatisticLine>
+      <StatisticLine text={"bad "} value={bad}></StatisticLine>
+      <StatisticLine text={"all "} value={total}></StatisticLine>
+      <StatisticLine text={"average "} value={average}></StatisticLine>
+      <StatisticLine text={"positive "} value={positive + " %"}></StatisticLine>
+    </>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = good + neutral + bad;
-  const average = total ? (good - bad) / total : 0;
-  const positive = total ? (good * 100) / total : 0;
 
   return (
     <>
@@ -25,12 +38,7 @@ const App = () => {
       <Button onClick={() => setBad(bad + 1)} text="bad" />
 
       <h1>statistics</h1>
-      <StatisticLine text={"good "} value={good}></StatisticLine>
-      <StatisticLine text={"neutral "} value={neutral}></StatisticLine>
-      <StatisticLine text={"bad "} value={bad}></StatisticLine>
-      <StatisticLine text={"all "} value={total}></StatisticLine>
-      <StatisticLine text={"average "} value={average}></StatisticLine>
-      <StatisticLine text={"positive "} value={positive + " %"}></StatisticLine>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </>
   );
 };
